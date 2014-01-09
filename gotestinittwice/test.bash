5d99cc3041a26dfba4e22d3a83ac912a6e8fc9d5
Yukinari Toyota <xxseyxx@gmail.com>
2014-01-09 15:54:42 +0900

    add objdump result

--- gotestinittwice/test.bash
+++ gotestinittwice/test.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#
+
 go version
 go test -v github.com/t-yuki/mygosandbox/gotestinittwice/a
 gocov test -v github.com/t-yuki/mygosandbox/gotestinittwice/a
@@ -28,3 +30,16 @@ gocov test -v github.com/t-yuki/mygosandbox/gotestinittwice/a/c
 # --- PASS: TestFunc1 (0.00 seconds)
 # PASS
 # ok      github.com/t-yuki/mygosandbox/gotestinittwice/a/c       0.001s
+
+
+# go test -c github.com/t-yuki/mygosandbox/gotestinittwice/a
+# objdump -t -T a.test | grep "a.init"
+# Output:
+# objdump: a.test: not a dynamic object
+# 00000000006538a5 g     O .noptrbss      0000000000000001 github.com/t-yuki/mygosandbox/gotestinittwice/a.initdone・
+# 00000000006538a0 g     O .noptrbss      0000000000000001 _/home/t-yuki/gopath.t-yuki/src/github.com/t-yuki/mygosandbox/gotestinittwice/a.initdone・
+# 000000000041c230 g     F .text  00000000000002ba runtime.symtabinit
+# 0000000000493680 g     F .text  00000000000000b5 _/home/t-yuki/gopath.t-yuki/src/github.com/t-yuki/mygosandbox/gotestinittwice/a.init・1
+# 0000000000493740 g     F .text  000000000000004b _/home/t-yuki/gopath.t-yuki/src/github.com/t-yuki/mygosandbox/gotestinittwice/a.init
+# 00000000004af820 g     F .text  00000000000000b5 github.com/t-yuki/mygosandbox/gotestinittwice/a.init・1
+# 00000000004af8e0 g     F .text  000000000000004b github.com/t-yuki/mygosandbox/gotestinittwice/a.init
