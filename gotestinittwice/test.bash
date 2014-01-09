923f0a3be045e366e64eafaf51f31e89227339fa
Yukinari Toyota <xxseyxx@gmail.com>
2014-01-09 15:26:25 +0900

    add c test

--- gotestinittwice/test.bash
+++ gotestinittwice/test.bash
@@ -1,24 +1,30 @@
 #!/bin/bash
 
 go version
-go test -v github.com/t-yuki/mygosandbox/gotestinittwice/...
-gocov test -v github.com/t-yuki/mygosandbox/gotestinittwice/...
+go test -v github.com/t-yuki/mygosandbox/gotestinittwice/a
+gocov test -v github.com/t-yuki/mygosandbox/gotestinittwice/a
 
-# the above may cause panic
-
+# Output:
 # go version go1.2 linux/amd64
-# 2014/01/09 15:01:53 init a
-# 2014/01/09 15:01:53 init b
-# 2014/01/09 15:01:53 init a
-# 2014/01/09 15:01:53 init a_test
+# 2014/01/09 15:24:51 init a
+# 2014/01/09 15:24:51 init b
+# 2014/01/09 15:24:51 init a
+# 2014/01/09 15:24:51 init a_test
 # === RUN TestFunc1
 # --- PASS: TestFunc1 (0.00 seconds)
 # PASS
 # ok      github.com/t-yuki/mygosandbox/gotestinittwice/a 0.001s
-# 2014/01/09 15:01:53 init a
-# 2014/01/09 15:01:53 init b
-# 2014/01/09 15:01:53 init b_test
+
+
+go test -v github.com/t-yuki/mygosandbox/gotestinittwice/a/c
+gocov test -v github.com/t-yuki/mygosandbox/gotestinittwice/a/c
+
+# Output:
+# 2014/01/09 15:25:35 init a
+# 2014/01/09 15:25:35 init b
+# 2014/01/09 15:25:35 init c
+# 2014/01/09 15:25:35 init c_test
 # === RUN TestFunc1
 # --- PASS: TestFunc1 (0.00 seconds)
 # PASS
-# ok      github.com/t-yuki/mygosandbox/gotestinittwice/a/b       0.001s
+# ok      github.com/t-yuki/mygosandbox/gotestinittwice/a/c       0.001s
